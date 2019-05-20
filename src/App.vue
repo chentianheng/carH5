29b8441a3b97ea0fd7b1060d3ce174987a366abf
huangchengjieGH <1062743913@qq.com>
2019-05-20 12:57:26 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -16,23 +16,6 @@
         },
         methods:{
             wxConfig () {
-                var myDate = new Date()
-                // send({
-                //     url:  "/api/wx/jsticket?url=" + encodeURIComponent(location.href) + '&aa=' + myDate,
-                //     method: 'get'
-                // }).then((res) => {
-                //     var jsTicket = res.data
-                //     wx.config({
-                //         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                //         appId: jsTicket.appId, // 必填，公众号的唯一标识
-                //         timestamp: jsTicket.timestamp, // 必填，生成签名的时间戳
-                //         nonceStr: jsTicket.nonceStr, // 必填，生成签名的随机串
-                //         signature: jsTicket.signature,// 必填，签名，见附录1
-                //         jsApiList: ['hideOptionMenu', 'chooseWXPay','updateAppMessageShareData', 'updateTimelineShareData', 'onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-                //     });
-                // }).catch((err) => {
-                //     console.error(err);
-                // });
                 Drive.prototype.jsTicket().then(res => {
                     let list = res.data.data || [];
                     var jsTicket = list
--- src/drive/index/index.vue
+++ src/drive/index/index.vue
@@ -13,13 +13,73 @@
         name: "home",
         data () {
             return {
-
+                copyUrl: 'https://jiayi.yf-gz.cn/?superiorId=" + customer.getId() + "#/my',
+                shareImg: 'https://zhaocha.yf-gz.cn/file/1547712343646_1f34c44062c690edeacba339a53a07f1.png'
             }
         },
         created () {
             this.getMember()
+            this.share()
         },
         methods:{
+            share () {
+                var that = this
+                this.option = {
+                    title: '嘉壹科技', // 分享标题, 请自行替换
+                    desc: '一站式互联网金融信息服务平台，免高额中介服务费，学习掌握贷款黑科技，最高200000极速下款，一键提交，最快10分钟下款！！！', // 分享描述, 请自行替换
+                    link: that.copyUrl, // 分享链接，根据自身项目决定是否需要split
+                    imgUrl: that.shareImg // 分享图标, 请自行替换，需要绝对路径
+                }
+                wx.onMenuShareTimeline({
+                    title: that.option.title, // 分享标题
+                    link: that.option.link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: that.option.imgUrl, // 分享图标
+                    success () {
+                        alert('分享朋友圈成功')
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel () {
+                        // 用户取消分享后执行的回调函数
+                    }})
+                wx.onMenuShareAppMessage({
+                    title: that.option.title, // 分享标题
+                    desc: that.option.desc, // 分享描述
+                    link: that.option.link,
+                    imgUrl: that.option.imgUrl, // 分享图标
+                    success: function () {
+                        alert('分享给朋友成功')
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
+                })
+                wx.updateTimelineShareData({
+                    title: '嘉壹科技:一站式互联网金融服务平台，免高额中介服务费，最高20万极速下款。', // 分享标题
+                    link: that.option.link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                    imgUrl: that.option.imgUrl, // 分享图标
+                    success () {
+                        // alert('分享朋友圈成功')
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel () {
+                        // 用户取消分享后执行的回调函数
+                    }
+                })
+                wx.updateAppMessageShareData({
+                    title: that.option.title, // 分享标题
+                    desc: that.option.desc, // 分享描述
+                    link: that.option.link,
+                    imgUrl: that.option.imgUrl, // 分享图标
+                    success: function () {
+                        // alert('分享给朋友成功')
+                        // 用户确认分享后执行的回调函数
+                    },
+                    cancel: function () {
+                        // 用户取消分享后执行的回调函数
+                    }
+                })
+            },
             getMember(){
                 const that = this;
                 Drive.prototype.getMember().then(res => {
